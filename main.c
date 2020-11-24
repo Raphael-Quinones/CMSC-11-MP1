a6da968954a78eaae3131365237e4dcd6ac428ac
Raphael Quiñones <rpquinones@up.edu.ph>
2020-11-24 22:42:34 +0800

    Added features for error handling
    Still no Core Functionality

--- main.c
+++ main.c
@@ -5,14 +5,27 @@ int main() {
     char arr[12] = {'B','D','Z','X','W','M','O','S','P','H','G','E'};
     char opt;
 
-    while (opt != '0') {
+    while (opt != '0') { // Makes code re-run when user doesn't define opt as '0'
+        // MAIN MENU -
         printf("Menu:\n");
         printf("1. Search the Array \n");
         printf("2. Delete the Array \n");
         printf("3. Sort Content of the Array\n");
         printf("0. Exit\n");
+        printf("*--* Please take note that the program only takes first character in your input* --*\n");
         printf("Choose the typing number: ");
         scanf(" %c", &opt);
+
+        while ( getchar() != '\n' ); //Remove extra characters from 'opt'. If this wasn't done, characters after first char will also be stored in opt
+        printf("You chose: %c\n", opt);
+        //NOTE
+        /*
+         I think this error handling is much more "convenient" for the user to use than just retrying to type the option again.
+         I also had the option of identifying \n (user enters nothing) as an error, but I just didn't decide it to be since, again,
+         It would be more "convenient" for the user
+         */
+
+        // - MAIN MENU
         switch (opt) {
             case '1':
                 printf("You pressed 1\n");
@@ -23,7 +36,10 @@ int main() {
             case '3':
                 printf("You pressed 3\n");
                 break;
-            default: //Checks if opt is in range
+            case '0': // Only used when User Enters 0 during first instance of program
+                printf("Program will now exit\n");
+                break;
+            default: //Checks if opt is in range or is a letter
                 printf("Sorry, the character you entered wasn't in the options displayed \nPlease Try Again.\n");
 
         }
