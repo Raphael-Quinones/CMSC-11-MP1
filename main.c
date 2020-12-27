fe8f93618296f9673a01bf927f4af4328e8ce7d4
Raphael Quiñones <rpquinones@up.edu.ph>
2020-12-27 16:46:29 +0800

    Final of Version 2
    Added 3 functions
    Search
    Delete
    Sort
    
    Same Functionality in output

--- main.c
+++ main.c
@@ -1,12 +1,72 @@
 #include <stdio.h>
 #include <ctype.h> //for toupper() function
+int search(char ch_search, int array_size, char const arr[], int* index){ //pass pointer of char arr since passing whole arrays are considered inefficient
+    for (int i=0; i<array_size; i++)
+    {
+        if (arr[i] == ch_search) // Executes when 'ch_search' has been found in char array
+        {
+            *index = i;//we can't return 2 values so we'll use pointers to access this processed value
+            return 1; //as stated in MP2 instructions
+        }
+        else if (i == (array_size - 1)) // Executes when loop has arrived to last char
+            return 0;
+    }
+}
 
+int delete(char ch_search, int* array_size, char arr[] ){ //"char* arr" is a character array
+    //remember "*" are dereferencing operators, means that it uses the pointer to find the value of the variable
+    for (int k=0;k<*array_size;k++)//searches from start of array
+    {
+        if(ch_search == arr[k])//Start process when 'ch_search' has been found
+        {
+            for (;k<*array_size;k++)
+            {
+                arr[k] = arr[k+1];//Remove 'ch_search' and shift left all elements after arr[j]
+                if (k == (*array_size-1))
+                    arr[k] = '\0';//Last item of array is considered none
+            }
+            *array_size = *array_size - 1; //Update array size. Minus one since one element has been deleted
+            return 1;
+        }
+        else if (k == *array_size-1)
+            return 0;
+    }
+
+}
+
+void sort(char arr[], int array_size){
+    char tmp;
+    for (int i=0; i<array_size; i++)
+        for(int j=i+1; j<array_size; j++)
+            if(arr[j] <arr[i])
+            {
+                tmp = arr[i];       //Sorting
+                arr[i] = arr[j];    //Sorting
+                arr[j] = tmp;       //Sorting
+            }
+}
 
 int main() {
-    //PRE-SET ARRAY
-    char arr[12] = {'B','D','Z','X','W','M','O','S','P','H','G','E'};
-    char opt, search, tmp;
-    int array_size = 12, k=0;
+    //THIS CODE BLOCK INITIATES ARRAY TO BE USED
+    //Temporary array for reading all lines in a file
+    //A text file with newlines is a 2d array
+    char arr[12];
+    char line[12][1]; //12 rows since there are 12 rows in input file and 1 column for each character
+    FILE *fptr;
+    int j = 0;
+    fptr = fopen("INPUT.txt", "r");
+    while(fgets(line[j], 3, fptr))//1 doesn't work, 2 doesn't work. Finally 3 works. This was only supposed to get the first character of each line in the file
+        j++;
+    //transfer all chars to own array
+    for (int k = 0; k<12; k++)
+        arr[k] = line[k][0];
+    fclose(fptr);
+
+
+
+
+    char opt = '+', ch_search;//opt is initialized as '+' arbitrarily to avoid getting a '0' randomly when main is initialized
+    int array_size = 12, index;
 
     while (opt != '0') { // Makes code re-run when user doesn't define opt as '0'
         // MAIN MENU -
@@ -33,24 +93,18 @@ int main() {
 
         // - MAIN MENU
         switch (opt) {
-            case '1'://Linear Search
+            case '1'://Linear ch_search
                 printf("You pressed 1\n"
                        "Input your letter to search: \n");
-                scanf(" %c", &search);
+                scanf(" %c", &ch_search);
                 fflush(stdin); //Clears buffer to only accept one character as input
-                search = toupper(search);//Transforms user input to upper
-                printf("\nInput = %c\n", search);
+                ch_search = toupper(ch_search);//Transforms user input to upper
+                printf("\nInput = %c\n", ch_search);
                 /* CORE */
-                for (int i=0; i<array_size; i++)
-                {
-                    if (arr[i] == search) // Executes when 'search' has been found in char array
-                    {
-                        printf("%c found at position %d\n",arr[i],i);
-                        break;
-                    }
-                    else if (i == (array_size - 1)) // Executes when loop has arrived to last char
-                        printf("Input not found in array\n");
-                }
+                if (search(ch_search, array_size, arr, &index)) //Search Function
+                    printf("%c found at position %d\n",arr[index],index);   //Print if function returns 1
+                else
+                    printf("Input not found in array\n"); //print if function returns 0
                  /* CORE */
                 break;
             case '2':
@@ -60,33 +114,20 @@ int main() {
                 for (int i = 0;i<array_size;i++)
                     printf("%c ", arr[i]);
                 printf("\n--Your input will be converted to uppercase--\n");
-                printf("\nEnter search: \n");
-                scanf(" %c", &search);
+                printf("\nEnter letter to be deleted: \n");
+                scanf(" %c", &ch_search);
                 fflush(stdin); //Clears buffer to only accept one character as input
-                search = toupper(search);//Transforms user input to upper
-                printf("\nYour input was: %c\n", search);
+                ch_search = toupper(ch_search);//Transforms user input to upper
+                printf("\nYour input was: %c\n", ch_search);
                 /* CORE */
-                //Use Original Array to save Memory
-                for (k;k<array_size;k++)//Searches from start of array
+                if (delete(ch_search, &array_size, arr)) //Search Function
                 {
-                    if(search == arr[k])//Start process when 'search' has been found
-                    {
-                        for (k;k<array_size;k++)
-                        {
-                            arr[k] = arr[k+1];//Remove 'search' and shift left all elements after arr[j]
-                            if (k == (array_size-1))
-                                arr[k] = NULL;//Last item of array is considered none
-                        }
-                        array_size = array_size - 1; //Update array size. Minus one since one element has been deleted
-                        printf("\nAfter Deletion:\n");
-                        for (int i=0;i<array_size;i++)
-                            printf("%c ", arr[i]);
-                        break;
-                    }
-                    else if (k == array_size-1)
-                        printf("\nSorry your input can't be found\n");
+                    printf("\nAfter Deletion:\n");
+                    for (int i = 0; i < array_size; i++)
+                        printf("%c ", arr[i]);
                 }
-                k=0; //restart value of k for repetitive instances
+                else
+                    printf("\nSorry your input can't be found\n");
                 /* CORE */
                 break;
             case '3':
@@ -97,14 +138,7 @@ int main() {
                 printf("Before sorting: \n");
                 for (int i=0; i<array_size; i++)
                     printf("%c ", arr[i]);
-                for (int i=0; i<array_size; i++)
-                    for(int j=i+1; j<array_size; j++)
-                        if(arr[j] <arr[i])
-                        {
-                            tmp = arr[i];       //Sorting
-                            arr[i] = arr[j];    //Sorting
-                            arr[j] = tmp;       //Sorting
-                        }
+                sort(arr, array_size);
                 /* CORE */
                 printf("\nAfter sorting: \n");
                 for (int i=0; i<array_size; i++)
@@ -124,5 +158,16 @@ int main() {
             fflush(stdin);
         }
     }
+
+    //Write data to "OUTPUT.txt"
+    FILE *outputptr;
+    outputptr = fopen("OUTPUT.txt", "w");
+    fputs("Array Me! ver 2.0 project by: Raphael Quinones\n"
+          "The final array content:\n", outputptr);
+    for (int i = 0; i<array_size; i++){
+        fputc(arr[i], outputptr);
+        fputs(" ", outputptr);
+    }
+    fclose(outputptr);
     return 0;
 }
